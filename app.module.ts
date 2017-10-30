e7ec8bcc7731faa618e1b18831563ff3bcd75a5c
Quanghai32 <quanghai.dt4@gmail.com>
2017-10-30 18:13:51 +0700

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,13 @@
+import {ChartModule} from 'angular2-highcharts'; 
+import {HighchartsStatic} from 'angular2-highcharts/dist/HighchartsService';
+
+declare var require: any;
+export function highchartsFactory() { 
+const hc = require('highcharts/highstock');
+const dd = require('highcharts/modules/exporting');
+dd(hc); return hc; 
+}
+
+//Section: Imports imports:[ChartModule],
+
+//Section: Providers providers:[ ... { provide: HighchartsStatic, useFactory: highchartsFactory } ]
